bec925d3ffc543ba433b15a06680c9f44ca6db9e
fuchaosz <chaogemail@126.com>
2016-08-02 17:53:15 +0800

    第一版基于AVNativeWindow播放成功

---
+++ obj/local/armeabi/objs/VideoPlayer/videoplayer.o.d
@@ -0,0 +1,113 @@
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/obj/local/armeabi/objs/VideoPlayer/videoplayer.o: \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/videoplayer.c \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/com_fuchao_ffmpegandroidplayer_VideoPlayer.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavcodec/avcodec.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/samplefmt.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/avutil.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/common.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/attributes.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/macros.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/version.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/avconfig.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/mem.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/error.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/rational.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/mathematics.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/intfloat.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/log.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/pixfmt.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/attributes.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/avutil.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/buffer.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/cpu.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/channel_layout.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/dict.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/frame.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/buffer.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/dict.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/samplefmt.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/log.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/pixfmt.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/rational.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavcodec/version.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/version.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavformat/avformat.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavformat/avio.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/common.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavformat/version.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libswscale/swscale.h \
+ /home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libswscale/version.h
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/com_fuchao_ffmpegandroidplayer_VideoPlayer.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavcodec/avcodec.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/samplefmt.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/avutil.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/common.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/attributes.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/macros.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/version.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/avconfig.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/mem.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/error.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/rational.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/mathematics.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/intfloat.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/log.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/pixfmt.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/attributes.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/avutil.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/buffer.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/cpu.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/channel_layout.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/dict.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/frame.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/buffer.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/dict.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/samplefmt.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/log.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/pixfmt.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/rational.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavcodec/version.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/version.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavformat/avformat.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavformat/avio.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavutil/common.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libavformat/version.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libswscale/swscale.h:
+
+/home/fuchao/workspace_adt/FFmpegAndroidPlayerEclipse/jni/include/libswscale/version.h:
---
+++ src/com/fuchao/ffmpegandroidplayer/MainActivity.java
@@ -0,0 +1,53 @@
+package com.fuchao.ffmpegandroidplayer;
+
+import android.app.Activity;
+import android.content.res.ObbInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	public static final String TAG = "VideoPlayer";
+	public static final String PATH = "/mnt/sdcard/a.flv";
+	private SurfaceView surfaceView;
+	private SurfaceHolder surfaceHolder;
+	private VideoPlayer videoPlayer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		this.surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+		this.surfaceHolder = surfaceView.getHolder();
+		// 创建视屏播放器
+		this.videoPlayer = new VideoPlayer(PATH);
+		this.surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+			@Override
+			public void surfaceCreated(SurfaceHolder surfaceHolder) {
+				final SurfaceHolder holder = surfaceHolder;
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						// SurfaceView创建完毕后就可以开始播放了
+						videoPlayer.init(holder.getSurface());
+					}
+				}).start();
+			}
+
+			@Override
+			public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+			}
+
+			@Override
+			public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+				// SurfaceView销毁时停止播放
+				videoPlayer.stop();
+			}
+		});
+	}
+}
